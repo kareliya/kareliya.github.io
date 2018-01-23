bbbf376c91ec83cce9ed7ea72e09fc6af4c754ca
Митрошкин Михаил Иванович <m.mitroshkin@ren-tv.com>
2018-01-23 10:15:41 +0300

    Adding project files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    hideAddressBar();
+    window.addEventListener("orientationchange", function () {
+        hideAddressBar();
+    }, false);
+}
+
+function hideAddressBar() {
+    setTimeout(function () {
+        document.body.style.height = window.outerHeight + 'px';
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 1100);
+    }, 1000);
+    console.log("orientationchange");
+    return false;
+}
